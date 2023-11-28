c034d5a8ad8a1d1a86e7fd402e0ff1abf7ad8ea6
pczheng8 <pczheng8@gmail.com>
2023-11-27 23:25:44 -0500

    Initial commit

---
+++ src/diver/McDiver.java
@@ -0,0 +1,34 @@
+package diver;
+
+import game.*;
+
+
+/** This is the place for your implementation of the {@code SewerDiver}.
+ */
+public class McDiver implements SewerDiver {
+
+    /** See {@code SewerDriver} for specification. */
+    @Override
+    public void seek(SeekState state) {
+        // TODO : Look for the ring and return.
+        // DO NOT WRITE ALL THE CODE HERE. DO NOT MAKE THIS METHOD RECURSIVE.
+        // Instead, write your method (it may be recursive) elsewhere, with a
+        // good specification, and call it from this one.
+        //
+        // Working this way provides you with flexibility. For example, write
+        // one basic method, which always works. Then, make a method that is a
+        // copy of the first one and try to optimize in that second one.
+        // If you don't succeed, you can always use the first one.
+        //
+        // Use this same process on the second method, scram.
+    }
+
+    /** See {@code SewerDriver} for specification. */
+    @Override
+    public void scram(ScramState state) {
+        // TODO: Get out of the sewer system before the steps are used up.
+        // DO NOT WRITE ALL THE CODE HERE. Instead, write your method elsewhere,
+        // with a good specification, and call it from this one.
+    }
+
+}
