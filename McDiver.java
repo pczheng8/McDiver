772e93435346272d9a7b6fe6dc198a84797aa918
ahanbm <77903094+ahanbm@users.noreply.github.com>
2023-12-04 16:00:55 -0500

    Add files via upload

---
+++ McDiver.java
@@ -0,0 +1,59 @@
+package diver;
+
+import game.*;
+import graph.ShortestPaths;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.List;
+
+
+/** This is the place for your implementation of the {@code SewerDiver}.
+ */
+public class McDiver implements SewerDiver {
+
+    /** See {@code SewerDriver} for specification. */
+    @Override
+    public void seek(SeekState state) {
+        // TODO : Look for the ring and return.
+        // DO NOT WRITE ALL THE CODE HERE. DO NOT MAKE THIS METHOD RECURSIVE.
+        // Instead, write your method (it may be recursive) elsewhere, with a
+        // good specification, and call it from this one.
+        //
+        // Working this way provides you with flexibility. For example, write
+        // one basic method, which always works. Then, make a method that is a
+        // copy of the first one and try to optimize in that second one.
+        // If you don't succeed, you can always use the first one.
+        //
+        // Use this same process on the second method, scram.
+    }
+
+    /** See {@code SewerDriver} for specification. */
+    @Override
+    public void scram(ScramState state) {
+        // TODO: Get out of the sewer system before the steps are used up.
+        // DO NOT WRITE ALL THE CODE HERE. Instead, write your method elsewhere,
+        // with a good specification, and call it from this one.
+        ArrayList<Node> nodes = shortScram(state);
+
+        for (Node n : nodes) {
+            state.moveTo(n);
+        }
+    }
+
+    public ArrayList<Node> shortScram(ScramState state) {
+        ShortestPaths<Node, Edge> s = new ShortestPaths<>(
+                new Maze((Set<Node>) state.allNodes()));
+        s.singleSourceDistances(state.currentNode());
+
+        ArrayList<Node> nodes = new ArrayList<>();
+        List<Edge> edges = s.bestPath(state.exit());
+        Node current = state.currentNode();
+
+        for (Edge e : edges) {
+            nodes.add(e.source());
+        }
+
+        nodes.add(state.exit());
+        return nodes;
+    }
+}
