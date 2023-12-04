614927afe1678320a7bec13d9c08d3dcc942b21c
ahanbm <77903094+ahanbm@users.noreply.github.com>
2023-12-04 17:10:22 -0500

    Add files via upload

--- src/diver/McDiver.java
+++ src/diver/McDiver.java
@@ -1,6 +1,12 @@
 package diver;
 
 import game.*;
+import graph.ShortestPaths;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
 
 
 /** This is the place for your implementation of the {@code SewerDiver}.
@@ -21,7 +27,28 @@ public class McDiver implements SewerDiver {
         // If you don't succeed, you can always use the first one.
         //
         // Use this same process on the second method, scram.
+        dfsWalk(state, new HashSet<>());
     }
+
+    private boolean dfsWalk(SeekState state, Set<Long> vis) {
+        assert !vis.contains(state.currentLocation());
+        if (state.distanceToRing() == 0) {
+            return true;
+        }
+        long cur = state.currentLocation();
+        vis.add(cur);
+        for (NodeStatus node : state.neighbors()) {
+            if (!vis.contains(node.getId())) {
+                state.moveTo(node.getId());
+                if(dfsWalk(state, vis)) return true;
+                state.moveTo(cur);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * See {@code SewerDriver} for specification.
 
     /** See {@code SewerDriver} for specification. */
     @Override
@@ -29,6 +56,25 @@ public class McDiver implements SewerDiver {
         // TODO: Get out of the sewer system before the steps are used up.
         // DO NOT WRITE ALL THE CODE HERE. Instead, write your method elsewhere,
         // with a good specification, and call it from this one.
+        ArrayList<Node> nodes = shortScram(state);
+
+        for (Node n : nodes) {
+            state.moveTo(n);
+        }
     }
 
+    public ArrayList<Node> shortScram(ScramState state) {
+        ShortestPaths<Node, Edge> s = new ShortestPaths<>(
+                new Maze((Set<Node>) state.allNodes()));
+        s.singleSourceDistances(state.currentNode());
+
+        ArrayList<Node> nodes = new ArrayList<>();
+        List<Edge> edges = s.bestPath(state.exit());
+
+        for (Edge e : edges) {
+            nodes.add(e.destination());
+        }
+
+        return nodes;
+    }
 }
